c2553cff2ef4416697faafb8a96fae1d9e093129
Arhum <67113142+ArhumTheDeveloper@users.noreply.github.com>
2021-11-19 20:04:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
 require("dotenv").config();
 var socket = require("socket.io");
-const server = require('http').Server()
-const app = express()
-const io = ('socket.io')(server);
-const { v4: uuidV4 } = require('uuid')
+var http = require("http");
+const server = require('http').Server();
+const express = require('express');
+const app = express();
+const io = (socket)(server);
+const { v4: uuidV4 } = require('uuid');
 
-app.set('view-engine', 'ejs')
-app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
-})
+  res.redirect(`/${uuidV4()}`);
+});
 
 app.get('/:room', (req, res) => {
-  res.render(':room', {roomId: req.params.room})
-})
-
-server.listen(env.PORT)
+  res.render('room', {roomId: req.params.room});
+});
 
 io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
-    socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId)
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit('user-connected', userId);
 
     socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId)
-    })
-  })
-})
+      socket.to(roomId).broadcast.emit('user-disconnected', userId);
+    });
+  });
+});
+
+http.createServer(app).listen(3000);
